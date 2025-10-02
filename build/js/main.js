108e0194e93b9467d395cba0d477d5577d415a71
XenoKarma <andikapurnama1803@gmail.com>
2025-10-02 13:43:25 +0700

    second-commit

---
+++ build/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nama = "Andika Purnama";
+console.log(nama);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+let nama = "Andika Purnama"
+
+console.log(nama)
